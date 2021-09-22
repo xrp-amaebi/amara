5749fc793103538ec6ad940461b2057a2a5a0c39
xrp-amaebi <amaebiamara@gmail.com>
2021-09-22 16:47:28 +0100

    css implementations

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,14 +1,13 @@
 import { EntrySearch, Input } from "./style"
 
 export function Search({ placeholder, inputRef, onTextChange, searchCount, totalCount }){
-    console.log({ totalCount })
     const total = !totalCount ? "N/A" : totalCount
 
     return(
         <EntrySearch>
             <Input placeholder={placeholder} ref={inputRef} onChange={onTextChange} />
 
-            <div><span> TotalItems: {total}</span> <span>Search Results: </span>{searchCount}</div>
+            <div><span> TotalItems: {total} </span> <br/><span>Search Results: </span>{searchCount}</div>
         </EntrySearch>
     )
 }
